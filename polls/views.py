792824bd2c370bef50d3575ee973642a313914aa
docker <docker@gmail.com>
2025-03-04 08:44:13 +0000

    cambiado de lte a gt en polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -29,7 +29,7 @@ class DetailView(generic.DetailView):
         """
         Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(pub_date__lte=timezone.now())
+        return Question.objects.filter(pub_date__gt=timezone.now())
 
 
 class ResultsView(generic.DetailView):
